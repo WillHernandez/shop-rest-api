da1dbca63521f036542d2bae0b8613521b798615
Will Hernandez <w.hernandez.code@gmail.com>
2022-12-29 09:57:48 -0500

    removed morgan npm package. causing cyclic error

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 const app = express();
 const port = process.env.PORT || 4000;
 const cors = require('cors');
-const morgan = require('morgan');
 const productRoutes = require('./routes/productRoutes');
 const orderRoutes = require('./routes/orderRoutes');
 const userRoutes = require('./routes/userRoutes');
@@ -22,7 +21,6 @@ const connectDB = async () => {
 
 app.use(cors({ origin: "*" }));
 app.use(express.json());
-app.use(morgan('dev'));
 
 // routes
 app.use('/api/products', productRoutes);
@@ -31,7 +29,6 @@ app.use('/api/user', userRoutes);
 
 connectDB().then(()=> {
 	app.listen(port, () => {
-		// console.log(`MongoDb connected / Server running on port ${port}`);
 		console.log(`listening for requests`);
 	});
 })
