f69d2c10933df194ce83ed322de38e38c5abea26
Will Hernandez <w.hernandez.code@gmail.com>
2022-12-29 10:28:31 -0500

    removed dotenv error cyclic

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+// require('dotenv').config();
 const UserModel = require('../models/userModel');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
